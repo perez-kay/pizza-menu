4bcf7c3deaa9a7a01c1f07fda22f9699a6421675
Kayla Perez <perezkay@uw.edu>
2024-06-12 14:30:05 -0700

    created more basic components

--- src/index.js
+++ src/index.js
@@ -49,9 +49,31 @@ const pizzaData = [
 function App() {
   return (
     <>
-      <h1>Hello React!</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
       <Pizza />
     </>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()} We're currently open!</footer>
   );
 }
 
